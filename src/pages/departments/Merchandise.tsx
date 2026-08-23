39ceea16382d9167ae9984775126ca5db0783669
808aoooo <2876706634@qq.com>
2026-08-23 18:39:32 +0800

    debug

--- src/pages/departments/Merchandise.tsx
+++ src/pages/departments/Merchandise.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import "./Departments.css";
